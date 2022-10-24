d66d2e057b5d7e7dae44053efc3f46c27460eeef
FruityOkapi <josephbsimmerman@gmail.com>
2022-10-24 12:55:56 -0400

    added javascripting to make buttons out of the search history and some fetch requests that dont work because the API key isnt valid yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,49 @@
 var searchHistory = ['Phoenix'];
 var bttn = $('.search-confirm');
 var userInput = $('#user-input');
-var historyAppend = $('.search-history')
-var city = userInput.val();
-var OpenWeatherApiKey = '2be36905677e50f79135cc9a8f2a062b';
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +'&appid=' + OpenWeatherApiKey;
+var historyAppend = $('.search-history');
+var city = userInput;
+var lat;
+var lon;
+var APIKey = '51bd6ed6a7fd20bcc3f9e281517384f2';
+var queryURLLatLog = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + APIKey;
+var queryURLForecast = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey;
 function dispStart () {
     for (var i = 0; i < searchHistory.length; i++) {
         var options = $('<button>');
         options.text(searchHistory[i]);
         options.addClass('search-options');
-        options.click(function(){
-            // Set this to search the text of this button
+        options.click(function() {
+            city = options.text();
+            fetch(queryURLLatLog)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(lat) {
+                console.log(lat);
+            })
+            .then(function(lon){
+                console.log(lon)
+            })
         })
         historyAppend.append(options);
     }
+    
 }
 function searchBttn() {
 
 }
+function getLatLon() {
+    fetch(queryURLLatLog)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(name) {
+        console.log(name);
+    })
+}
 function grabWeather() {
-    fetch(queryURL);
+
 }
 function setHistory() {
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
